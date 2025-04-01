3299d2421305596b3784ea98d4c2dc08f22c527a
Pynckels <filip.pynckels@telenet.be>
2025-04-01 23:49:12 +0200

    Clean up init.c

--- init/src/init.c
+++ init/src/init.c
@@ -16,22 +16,26 @@
 // -----------------------------------------------------------------------------
 
 int main() {
-    char command[255];
-    int count;
+    char      command[255];
+    int       count;
+    pid_t     fork_result
     siginfo_t info;
 
     while (1) {
         sys_write(STDOUT, PROMPT, sizeof(PROMPT) - 1);
+
         count = sys_read(STDIN, command, sizeof(command));
-        if (count <= 0) continue;
+        if (count <= 0) continue;                               // If read error: loop
         command[count - 1] = 0;                                 // Replace '\n' with '\0'
-        pid_t fork_result = sys_fork();
+
+        fork_result = sys_fork();
         if (fork_result == 0) {
-            sys_execve(command, NULL, NULL);
-            sys_exit(0);
+            sys_execve(command, NULL, NULL);                    // Run command in child process
+            sys_exit(0);                                        // Exit child process
         } else {
-            sys_waitid(P_ALL, 0, &info, WEXITED, NULL);
+            sys_waitid(P_ALL, 0, &info, WEXITED, NULL);         // Wait for all child processes
         }
     }
-    sys_exit(0);
+
+    sys_exit(0);                                                // This should never be reached
 }
